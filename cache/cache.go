92658e62aa8f348de3e77e706be890625386da44
pj <pj@pjebs.com.au>
2018-01-06 15:40:41 +1100

    Updated cache package

--- cache/cache.go
+++ cache/cache.go
@@ -1,8 +1,8 @@
 package cache
 
 import (
+	"context"
 	"encoding/gob"
-	"golang.org/x/net/context"
 	"log"
 	"time"
 
@@ -15,20 +15,36 @@ import (
 
 type SlowRetrieve func(ctx context.Context) (interface{}, error)
 
+type CacheOptions struct {
+	// DisableCacheUsage disables the cache. (Usually for debugging)
+	DisableCacheUsage bool
+	// UseFreshData will ignore content in the cache and pull fresh data.
+	// The pulled data will subsequently be saved in the cache
+	UseFreshData bool
+	//Turn on logging
+	Log bool
+}
+
 //Options:
 //param 1 (bool): disable caching. (Usually for debugging)
 //Param 2 (bool): Obtain fresh copy. Ignore content in cache but store fresh copy in cache.
 //NB: In order for Param 2 to be activated, param 1 must be false.
-func Remember(ctx context.Context, key string, expiration time.Duration, p SlowRetrieve, options ...bool) (interface{}, error) {
+func Remember(ctx context.Context, key string, expiration time.Duration, p SlowRetrieve, options ...CacheOptions) (interface{}, error) {
 
-	disableCache := false
-	fresh := false
+	var (
+		disableCache bool
+		fresh        bool
+		mustLog      bool
+	)
 
-	if len(options) != 0 {
-		disableCache = options[0]
-		if len(options) >= 2 {
-			fresh = options[1]
-		}
+	if appengine.IsDevAppServer() {
+		mustLog = true
+	}
+
+	if options != nil {
+		disableCache = options[0].DisableCacheUsage
+		fresh = options[0].UseFreshData
+		mustLog = options[0].Log
 	}
 
 	//For debugging, you can disable cache
@@ -39,7 +55,7 @@ func Remember(ctx context.Context, key string, expiration time.Duration, p SlowR
 	var v interface{}
 
 	if fresh {
-		if appengine.IsDevAppServer() {
+		if mustLog {
 			log.Println("\x1b[31mGrabbing (fresh) from SlowRetrieve key:", key, "\x1b[39;49m")
 		}
 		goto fresh
@@ -48,12 +64,12 @@ func Remember(ctx context.Context, key string, expiration time.Duration, p SlowR
 	//Check if item exists
 	if _, err := memcache.Gob.Get(ctx, key, &v); err == nil {
 		//Item exists in cache
-		if appengine.IsDevAppServer() {
+		if mustLog {
 			log.Println("\x1b[36mFound in Cache key:", key, "\x1b[39;49m")
 		}
 		return v, nil
 	} else {
-		if appengine.IsDevAppServer() {
+		if mustLog {
 			log.Println("\x1b[31mGrabbing from SlowRetrieve key:", key, err, "\x1b[39;49m")
 		}
 	}
@@ -70,7 +86,7 @@ fresh:
 		}()
 		gob.Register(itemToStore)
 	}(itemToStore)
-	
+
 	//Store item in Cache
 	item := &memcache.Item{
 		Key:        key,
@@ -81,7 +97,7 @@ fresh:
 	err = memcache.Gob.Set(ctx, item)
 	if err != nil {
 		//Memcache storage failed
-		if appengine.IsDevAppServer() {
+		if mustLog {
 			log.Println("\x1b[31mCould not store item to memcache key:", key, err, itemToStore, "\x1b[39;49m")
 		}
 	}
---
+++ cache/hash.go
@@ -0,0 +1,48 @@
+package cache
+
+import (
+	"fmt"
+	"hash/crc32"
+	"runtime"
+)
+
+type Key string
+
+// CreateKey will generate a key based on the input arguments.
+// When prefix is true, the caller's name will be used to prefix the key in an attempt to make it unique.
+// The args can also be separated using sep.
+func CreateKey(prefix bool, sep string, args ...interface{}) Key {
+	var output string
+
+	if prefix {
+		pc, file, line, ok := runtime.Caller(1)
+		if !ok {
+			return Key(fmt.Sprint(args...))
+		}
+		details := runtime.FuncForPC(pc)
+		output = fmt.Sprintf("%s_%s_%d_", details.Name(), file, line)
+	}
+
+	if sep == "" {
+		output = output + fmt.Sprint(args...)
+	} else {
+		for i, v := range args {
+			if i != 0 {
+				output = output + sep
+			}
+			output = output + fmt.Sprint(v)
+		}
+	}
+
+	return Key(output)
+}
+
+// Hash returns a crc32 version of key
+func (k Key) Hash() Key {
+	return Key(fmt.Sprintf("%08x\n", crc32.ChecksumIEEE([]byte(k))))
+}
+
+// String returns a string version of the key
+func (k Key) String() string {
+	return string(k)
+}
